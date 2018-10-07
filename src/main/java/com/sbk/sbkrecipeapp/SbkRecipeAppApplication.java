efaf7727e7a465dfc482ca4e395c8aafe10b23d6
Thato Seboko <thato.seboko@gmail.com>
2018-10-07 22:47:29 +0200

    recipe project

---
+++ src/main/java/com/sbk/sbkrecipeapp/SbkRecipeAppApplication.java
@@ -0,0 +1,12 @@
+package com.sbk.sbkrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbkRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbkRecipeAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sbk/sbkrecipeapp/SbkRecipeAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sbk.sbkrecipeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbkRecipeAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
