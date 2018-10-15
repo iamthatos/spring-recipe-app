5825ae4936f34207d0de643459151530f3419625
Thato Seboko <thato.seboko@gmail.com>
2018-10-15 14:46:09 +0200

    Mockito Arg Capture

---
+++ src/test/java/com/sbk/sbkrecipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,62 @@
+package com.sbk.sbkrecipeapp.controllers;
+
+import com.sbk.sbkrecipeapp.domain.Recipe;
+import com.sbk.sbkrecipeapp.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class IndexControllerTest {
+    
+    IndexController indexController;
+    
+    @Mock
+    RecipeService recipeService;
+    
+    @Mock
+    Model model;
+    
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() throws Exception {
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
+        String viewName = indexController.getIndexPage(model);
+
+
+        //then
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
+        
+    }
+
+}
